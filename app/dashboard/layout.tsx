94b3bc6a25a92dbdf3be7555f88cd66a2225b99f
Arthur Ming <guobin.ming@pwc.com>
2024-08-12 21:21:54 +0800

    feat: create layouts and pages

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import clsx from "clsx";
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={clsx("flex h-screen md:flex-row md:overflow-hidden")}>
+      <div className={clsx("w-full flex-none md:w-64")}>
+        <SideNav />
+      </div>
+      <div className={clsx("flex-grow p-6 md:overflow-y-auto md:p-12")}>
+        {children}
+      </div>
+    </div>
+  );
+}
